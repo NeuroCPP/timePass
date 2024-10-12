29dd6220758082826087e7511fc3b677c3c3c716
VIBHU SAHU <77403072+VibhuSahu@users.noreply.github.com>
2024-10-12 13:37:52 +0000

    Add basic function

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -121,3 +121,15 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+
+# Static files (CSS, JavaScript, images)
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+# Media files (uploaded user files)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / "media"
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
